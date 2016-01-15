643cfb82346bb6be2bd5a34ffec60f70f08cf181
Sereda-Fazen <fazen7@mail.ru>
2016-01-15 16:36:24 +0200

    content1

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -6,8 +6,12 @@ class TestCest
 
 
 
-    function checkContentLinks(Step\Acceptance\HomeSteps $I,\Page\Header $homePage){
-        $I->getCheckFeaturedBrands();
+    function footerSubscribe(\Page\Header $homePage)
+    {
+        $homePage->subscribeEmptyField();
+        $homePage->subscribeInvalidEmail('123qwerty');
+        $homePage->subscribeIsNotEmail('dev.denimio@yahoo.com');
+        $homePage->subscribeSuccess('johndoe@domain.com');
     }
 
 
