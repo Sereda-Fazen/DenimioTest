c42e4b7bd0569452c584bb9c2394a6363e8cfc3d
Sereda-Fazen <fazen7@mail.ru>
2016-01-21 10:18:00 +0200

    footer-2

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -3,22 +3,9 @@ use \Step\Acceptance;
 
 class TestCest {
 
-    function footerGetClosers(Step\Acceptance\HomeSteps $I,\Page\HomePage $homePage)
-    {
-        $homePage->homeFooterFacebook();
-        $I->getSecondOpen();
-
-        $homePage->homeFooterTwiter();
-        $I->getSecondOpen();
-        $homePage->assertCheckTwitter();
-
-        $homePage->homeFooterPinterest();
-        $I->getSecondOpen();
-        $homePage->assertCheckPinterest();
-
-        $homePage->homeFooterInstagram();
-        $I->getSecondOpen();
-        $homePage->assertCheckInstagram();
+    function headerCurrencyCheck(Step\Acceptance\HomeSteps $I, \Page\HomePage $homePage){
+        $homePage->home();
+        $I->getCurrency();
     }
 
 
