858acd11ee34e909450318b71172b104dc79f4e4
Sereda-Fazen <fazen7@mail.ru>
2016-01-26 10:54:49 +0200

    fix-14

--- tests/_support/Page/ForgotPass.php
+++ tests/_support/Page/ForgotPass.php
@@ -9,6 +9,17 @@ class ForgotPass
     public static $subSave = 'div.buttons-set > button.button > span > span';
     public static $msg = 'li.success-msg';
 
+    //delete
+
+    public static $URL2 = 'https://mail.yahoo.com';
+    public static $email = '//*[@id="login-username"]';
+    public static $pass = '//*[@id="login-passwd"]';
+    public static $enter = '//*[@id="login-signin"]';
+    public static $waitMsg = '//*[@class="icon info info-real info-unread "]';
+    public static $clickOldMsg = 'div.cbox > input';
+    public static $delete = '#btn-delete > span.icon-text';
+    public static $msgRemove = '//p[@class="empty-folder-footer"]';
+
     protected $tester;
 
     public function __construct(\AcceptanceTester $I) {
@@ -22,6 +33,18 @@ class ForgotPass
         $I->fillField(self::$mail, $mailPass);
         $I->click(self::$subSave);
         $I->see('If there is an account associated with denimio_test@yahoo.com you will receive an email with a link to reset your password.', self::$msg);
+    }
+
+    public function deleteMsg(){
+        $I = $this->tester;
+        $I->amOnUrl(self::$URL2);
+
+        $I->waitForElementNotVisible(self::$waitMsg,5);
+        $I->wait(3);
+        $I->click(self::$clickOldMsg);
+        $I->waitForElementVisible(self::$delete);
+        $I->click(self::$delete);
+        $I->waitForElement(self::$msgRemove);
     }
 
 
--- tests/acceptance/03_ForgotPassCest.php
+++ tests/acceptance/03_ForgotPassCest.php
@@ -9,11 +9,17 @@ class ForgotPassCest {
     {
         $forgotPage->forgot('denimio_test@yahoo.com');
     }
+
     function enterNewPass (Step\Acceptance\ForgotPassSteps $I)
     {
         $I->gMailAuth();
         $I->remoteWindow();
         $I->newPass();
+
+    }
+
+    function deleteOldMsg(Step\Acceptance\LoginSteps $I, Page\ForgotPass $deleteMsg){
+        $deleteMsg->deleteMsg();
 
     }
 /*
