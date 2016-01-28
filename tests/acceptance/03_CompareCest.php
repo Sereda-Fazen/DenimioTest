f8386fa0d1c0b6c61c462ccc08b33bf92ffb6d8a
Sereda-Fazen <fazen7@mail.ru>
2016-01-28 17:32:55 +0200

    fix-25

--- tests/acceptance/03_CompareCest.php
+++ tests/acceptance/03_CompareCest.php
@@ -12,21 +12,25 @@ class CompareCest
          * @param \Page\Compare $blogPage
          * @param \Step\Acceptance\ItemsSteps $I
          */
-    /*
-        function checkCompareTwoItems(\Page\Compare $blogPage, \Step\Acceptance\ItemsSteps $I)
-        {
-            $I->addToCartForCompare();
-            $I->remoteWindow();
-            $I->compareClosePage();
-    }
-    */
-
-
     function checkCompareAndAddToCart(\Page\Compare $blogPage, \Step\Acceptance\ItemsSteps $I)
     {
         $I->addToCartForCompare();
         $I->remoteWindow();
         $I->compareAddToCart();
+    }
+
+    function checkCompareTwoItems(\Page\Compare $blogPage, \Step\Acceptance\ItemsSteps $I)
+    {
+        $I->addToCartForCompare();
+        $I->remoteWindow();
+        $I->compareClosePage();
+    }
+
+    function checkAddToWishListGuestUser(\Page\Compare $blogPage, \Step\Acceptance\ItemsSteps $I)
+    {
+        $I->compareAddToCart3();
+        $I->compareAddToWishListGuestUser();
+        $I->compareAddToCartFromMyComparison();
     }
 
     function checkRemoveItemsFromComparePage(\Page\Compare $blogPage, \Step\Acceptance\ItemsSteps $I)
