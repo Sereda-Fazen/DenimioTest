fb465c7ee17cb0ea9a8008fe07e70a644c40ec7d
Sereda-Fazen <fazen7@mail.ru>
2016-02-18 16:26:58 +0200

    fix-9

--- tests/_support/Step/Acceptance/MyShoppingCartSteps.php
+++ tests/_support/Step/Acceptance/MyShoppingCartSteps.php
@@ -23,28 +23,39 @@ class MyShoppingCartSteps extends \AcceptanceTester
     {
         $I = $this;
         $I->checkAccessories();
+
         $blockAcc1 = rand(1, count($I->grabMultiple('//div[@class="category-products"]/ul[1]/li')));
         $blockAcc2 = rand(1, count($I->grabMultiple('//div[@class="category-products"]/ul')));
         $I->wait(2);
-        $I->moveMouseOver('//div[@class="category-products"]/ul[' . $blockAcc2 . ']/li[' . $blockAcc1 . ']//div/div/div/div/button');
-        $I->click('//div[@class="category-products"]/ul[' . $blockAcc2 . ']/li[' . $blockAcc1 . ']//div/div/div/div/button');
+        $test = count($I->grabMultiple('//div[@class="category-products"]/ul[' . $blockAcc2 . ']/li[' . $blockAcc1 . ']//div/div/div/div/button'));
 
+        $I->moveMouseOver('//div[@class="category-products"]/ul[' . $blockAcc2 . ']/li[' . $blockAcc1 . ']');
+        $I->wait(2);
 
-        $I->waitForAjax(10);
-        $I->waitForElement('//div[@class="wrapper_box"]');
-        $I->click('//a[@id="shopping_cart"]');
-        $I->see('SHOPPING CART','h1');
-        $I->click('//td[@class="a-center"]/a');
-        $I->see('UPDATE CART','//button[@class="button btn-cart"]/span');
-        $I->fillField('//div[@class="add-to-cart"]/input', '2');
-        $I->click('//button[@class="button btn-cart"]/span');
-        $I->scrollUp(300);
-        $I->wait(3);
-        $I->see('2 items', 'a > span > span:first-child');
-        $I->click('a > span > span:first-child');
-        $I->fillField('input.input-text.qty','3');
-        $I->click('//button[@name="update_cart_action"]');
-        $I->see('3 items', 'a > span > span:first-child');
+        if ($test ==  true) {
+            $I->moveMouseOver('//div[@class="category-products"]/ul[' . $blockAcc2 . ']/li[' . $blockAcc1 . ']//div/div/div/div/button');
+            $I->click('//div[@class="category-products"]/ul[' . $blockAcc2 . ']/li[' . $blockAcc1 . ']//div/div/div/div/button');
+
+            $I->waitForAjax(10);
+            $I->waitForElement('//div[@class="wrapper_box"]');
+            $I->click('//a[@id="shopping_cart"]');
+            $I->see('SHOPPING CART', 'h1');
+            $I->click('//td[@class="a-center"]/a');
+            $I->see('UPDATE CART', '//button[@class="button btn-cart"]/span');
+            $I->fillField('//div[@class="add-to-cart"]/input', '2');
+            $I->click('//button[@class="button btn-cart"]/span');
+            $I->scrollUp(300);
+            $I->wait(3);
+            $I->see('2 items', 'a > span > span:first-child');
+            $I->click('a > span > span:first-child');
+            $I->fillField('input.input-text.qty', '3');
+            $I->click('//button[@name="update_cart_action"]');
+            $I->see('3 items', 'a > span > span:first-child');
+
+       } else {
+            $I->click('//div[@class="category-products"]/ul[' . $blockAcc2 . ']/li[' . $blockAcc1 . ']//div/div/div/ul/li[2]');
+        }
+
 
 
     }
