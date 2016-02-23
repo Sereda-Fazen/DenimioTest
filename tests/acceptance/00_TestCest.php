b83413a28b234a9aea083c9ba6b9938941545bb5
Sereda-Fazen <fazen7@mail.ru>
2016-02-23 10:02:35 +0200

    fixed-9

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -1,22 +1,18 @@
 <?php
 use \Step\Acceptance;
 
+/**
+ * @group test
+ */
 class TestCest {
 
 
 
-    function checkAddToMyCart(\Step\Acceptance\MyShoppingCartSteps $I)
-    {
-        $I->checkFunctionalInRandomOrder();
-
+    function loginSuccess(Step\Acceptance\Steps $I, \Page\Login $loginPage) {
+        $loginPage->login('denimio_test@yahoo.com', '123456');
+        $I->see('From your My Account Dashboard you have the ability to view','div.welcome-msg');
+        $loginPage->logout();
     }
-
-    function checkFunctionalForSecondItemShoppingCart(\Page\MyShoppingCart $shoppingPage, \Step\Acceptance\MyShoppingCartSteps $I)
-    {
-        $I->checkCouponAndGiftCard();
-
-    }
-
 
 
 
