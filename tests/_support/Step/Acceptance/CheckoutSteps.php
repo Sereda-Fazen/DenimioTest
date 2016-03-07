1b59867d9aa441860ca7e64d2661657309d68a70
Alex <sa@itsvit.org>
2016-03-07 11:08:33 +0200

    TJ-4 6

--- tests/_support/Step/Acceptance/CheckoutSteps.php
+++ tests/_support/Step/Acceptance/CheckoutSteps.php
@@ -167,7 +167,7 @@ class CheckoutSteps extends \AcceptanceTester
         $I->click('#onestepcheckout-button-place-order');
 
         $I->waitForElementVisible('li.error-msg');
-        $I->see('PayPal gateway has rejected request.','li.error-msg');
+        $I->see('Unable to communicate with PayPal gateway','li.error-msg');
 
     }
 
