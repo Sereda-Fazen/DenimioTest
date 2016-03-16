7a2d2725faceefb54dd4010cd090baa653dfe594
Alex <sa@itsvit.org>
2016-03-16 15:41:46 +0200

    comparison-6

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -4,18 +4,18 @@ use \Step\Acceptance;
 class TestCest
 {
 
-/*
+
     function checkAddToWishListGuestUser(\Page\Compare $blogPage, \Step\Acceptance\ItemsSteps $I)
     {
         $I->compareAddToCart3();
     }
-*/
+/*
     function checkCompareAndAddToCart(\Page\Compare $blogPage, \Step\Acceptance\ItemsSteps $I)
     {
         $I->addToCartForCompare();
 
     }
-
+*/
 
 
 }
