d72422d0f4b9f7730b173f5d902344cff839f916
Alex <sa@itsvit.org>
2016-03-21 10:49:26 +0200

    comparison-32

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -4,9 +4,16 @@ use \Step\Acceptance;
 class TestCest
 {
 
-    function checkGridAndList(\Page\Category $categoryPage, \Helper\Acceptance $I)
+    function forgotSuccess(Step\Acceptance\ForgotPassSteps $I, \Page\ForgotPass $forgotPage)
     {
-        $categoryPage->checkGridAndList();
+        $forgotPage->forgot('denimio_test@yahoo.com');
+    }
+    //
+    function enterNewPass (Step\Acceptance\ForgotPassSteps $I)
+    {
+        $I->gMailAuth();
+        $I->remoteWindow();
+        $I->newPass();
     }
 
 
