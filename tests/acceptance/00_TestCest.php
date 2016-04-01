541b81400632e28ebc9381615045a5caae7278a7
Alex <sa@itsvit.org>
2016-04-01 17:33:04 +0300

    update_for_prod-35

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -4,7 +4,10 @@ use \Step\Acceptance;
 class TestCest
 {
 
-
+    function forgotSuccess(Step\Acceptance\ForgotPassSteps $I, \Page\ForgotPass $forgotPage)
+    {
+        $forgotPage->forgot('denimio_test@yahoo.com');
+    }
 
     function enterNewPass (Step\Acceptance\ForgotPassSteps $I)
     {
@@ -12,6 +15,7 @@ class TestCest
         $I->remoteWindow();
         $I->newPass();
     }
+
 
 
 
--- tests/acceptance/01_ForgotPassCest.php
+++ tests/acceptance/01_ForgotPassCest.php
@@ -10,8 +10,14 @@ class ForgotPassCest {
         $forgotPage->forgot('denimio_test@yahoo.com');
     }
 
+    function enterNewPass (Step\Acceptance\ForgotPassSteps $I)
+    {
+        $I->gMailAuth();
+        $I->remoteWindow();
+        $I->newPass();
+    }
+
     /*
-       
         function deleteOldMsg(Step\Acceptance\LoginSteps $I, Page\ForgotPass $deleteMsg){
             $deleteMsg->deleteMsg();
 
