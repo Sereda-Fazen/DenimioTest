9f11de5bd446319ebb566382b6061a6672a0c8b4
Sereda-Fazen <fazen7@mail.ru>
2016-04-15 17:56:37 +0300

    update_for_prod-73

--- tests/acceptance/00_TestCest.php
+++ tests/acceptance/00_TestCest.php
@@ -6,12 +6,16 @@ use \Step\Acceptance;
  */
 class TestCest
 {
-    function checkCreateYourOwnShipping (Step\Acceptance\CheckoutSteps $I){
-        $I->createYouShipping();
+    function checkInputPrices(\Page\Category $categoryPage, \Step\Acceptance\CategorySteps $I)
+    {
+        $I->category();
+        $categoryPage->checkInputPrices('100', '300');
     }
 
-
-
+    function checkInputInvalidPrices(\Page\Category $categoryPage, AcceptanceTester $I)
+    {
+        $categoryPage->checkIsNotFindPrice('232', '232');
+    }
 
 
 }
